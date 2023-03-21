e2aec35a0db32ef4b9ad5ae89ad41ca48133e502
Vitalii <vitaliinnest@gmail.com>
2023-03-21 19:02:30 +0200

    Fixed CreateTenant validation

--- OrgansDelivery.BL/Validators/CreateTenantModelValidator.cs
+++ OrgansDelivery.BL/Validators/CreateTenantModelValidator.cs
@@ -18,14 +18,14 @@ public class CreateTenantModelValidator : AbstractValidator<CreateTenantModel>
         RuleFor(t => t.Name)
             .NotNull()
             .NotEmpty()
-            .Matches(GeneralConsts.STR_INT_REGEX)
+            .Matches(GeneralConsts.STR_INT_SPACE_REGEX)
             .Length(GeneralConsts.MIN_LENGTH,
                     GeneralConsts.MAX_LENGTH);
 
         RuleFor(t => t.Description)
             .NotNull()
             .NotEmpty()
-            .Matches(GeneralConsts.STR_INT_REGEX)
+            .Matches(GeneralConsts.STR_INT_SPACE_REGEX)
             .Length(GeneralConsts.MIN_LENGTH,
                     GeneralConsts.MAX_LENGTH);
     }
