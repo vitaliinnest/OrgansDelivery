b85f55c50fd3366a398af47b6a79955381571319
Vitalii <vitaliinnest@gmail.com>
2023-03-21 23:50:51 +0200

    code format

--- OrgansDelivery.BL/Services/AuthService.cs
+++ OrgansDelivery.BL/Services/AuthService.cs
@@ -90,7 +90,8 @@ public class AuthService : IAuthService
             return Result.Fail("User with given email already exists");
         }
 
-        if (registerRequest.InviteCode.HasValue && _inviteService.GetRegisterInvite(registerRequest) == null)
+        if (registerRequest.InviteCode.HasValue
+            && _inviteService.GetRegisterInvite(registerRequest) == null)
         {
             return Result.Fail("Email or invite code is invalid");
         }
