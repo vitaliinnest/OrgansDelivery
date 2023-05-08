3a543e599589edce28c8e30f404dd0dde9f6f664
Vitalii <vitaliinnest@gmail.com>
2023-05-08 17:30:31 +0300

    added sign in page

--- OrgansDelivery.Web/client-app/src/features/users/SignInPage.tsx
+++ OrgansDelivery.Web/client-app/src/features/users/SignInPage.tsx
@@ -1,70 +1,132 @@
-import { ErrorMessage, Form, Formik } from "formik";
+import React from "react";
+import { Formik } from "formik";
 import { observer } from "mobx-react-lite";
 import { useStore } from "../../app/stores/store";
-import * as yup from 'yup';
+import * as Yup from 'yup';
+import YupPassword from "yup-password";
+import { Login } from "../../app/models/user";
+import {
+    Avatar,
+    Box,
+    Button,
+    Container,
+    Grid,
+    Link,
+    TextField,
+    Typography,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+YupPassword(Yup);
 
-// const validationSchema = yup.object({
-//     email: yup
-//         .string().email().required(),
-//     password: yup
-//         .string("Enter your password")
-//         .min(8, "Password should be of minimum 8 characters length")
-//         .required("Password is required"),
-// });
-  
+const validationSchema = Yup.object({
+    email: Yup.string().email(),
+    password: Yup.string().required().min(8).minLowercase(1),
+});
+
+const initialValues: Login = {
+    email: "",
+    password: "",
+};
 
 const SignInPage = () => {
     const { userStore } = useStore();
+
     return (
-        <Formik
-            initialValues={{ email: "", password: "", error: null }}
-            onSubmit={(values, { setErrors }) =>
-                userStore
-                    .login(values)
-                    .catch((error) =>
-                        setErrors({ error: "Invalid email or password" })
-                    )
-            }
+        <Container
+            sx={{
+                marginTop: 12,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+            }}
+            maxWidth="sm"
         >
-            {({ handleSubmit, isSubmitting, errors }) => (
-                <Form
-                    className="ui form"
-                    onSubmit={handleSubmit}
-                    autoComplete="off"
-                >
-                    {/* <Header
-                        as="h2"
-                        content="Login to Reactivities"
-                        color="teal"
-                        textAlign="center"
-                    />
-                    <MyTextInput placeholder="Email" name="email" />
-                    <MyTextInput
-                        placeholder="Password"
-                        name="password"
-                        type="password"
-                    />
-                    <ErrorMessage
-                        name="error"
-                        render={() => (
-                            <Label
-                                style={{ marginBottom: 10 }}
-                                basic
-                                color="red"
-                                content={errors.error}
-                            />
-                        )}
-                    />
-                    <Button
-                        loading={isSubmitting}
-                        positive
-                        content="Login"
-                        type="submit"
-                        fluid
-                    /> */}
-                </Form>
-            )}
-        </Formik>
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+                Sign In
+            </Typography>
+            <Formik
+                initialValues={initialValues}
+                onSubmit={(creds) => userStore.login(creds)}
+                validationSchema={validationSchema}
+            >
+                {({
+                    values,
+                    isSubmitting,
+                    errors,
+                    touched,
+                    isValid,
+                    handleSubmit,
+                    handleChange,
+                }) => (
+                    <Box
+                        component="form"
+                        noValidate
+                        onSubmit={handleSubmit}
+                        sx={{ mt: 3 }}
+                    >
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <TextField
+                                    name="email"
+                                    label="Email Address"
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    onChange={handleChange}
+                                    value={values.email}
+                                    error={
+                                        touched.email && Boolean(errors.email)
+                                    }
+                                    helperText={touched.email && errors.email}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    onChange={handleChange}
+                                    value={values.password}
+                                    error={
+                                        touched.password &&
+                                        Boolean(errors.password)
+                                    }
+                                    helperText={
+                                        touched.password && errors.password
+                                    }
+                                />
+                            </Grid>
+                        </Grid>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign Up
+                        </Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body1">
+                                    Forgot password?
+                                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link href="#" variant="body1">
+                                    Don't have an account? Sign Up
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                )}
+            </Formik>
+        </Container>
     );
 }
 
