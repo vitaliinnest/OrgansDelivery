af3771e5349dbe96a69407c6b3f3088804401242
Vitalii <vitaliinnest@gmail.com>
2023-05-22 21:05:54 +0300

    added translation to the Container Info section

--- OrgansDelivery.Web/client-app/src/features/organs/OrganDashboard.tsx
+++ OrgansDelivery.Web/client-app/src/features/organs/OrganDashboard.tsx
@@ -5,10 +5,15 @@ import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import { observer } from "mobx-react-lite";
-import ConditionsLineChart, { ConditionsRecordUnit } from "../../app/layout/charts/ConditionsLineChart";
+import ConditionsLineChart, {
+    ConditionsRecordUnit,
+} from "../../app/layout/charts/ConditionsLineChart";
 import { grey } from "@mui/material/colors";
 import { Organ } from "../../app/models/organ";
-import { ConditionsRecord, ConditionsViolation } from "../../app/models/conditionsRecord";
+import {
+    ConditionsRecord,
+    ConditionsViolation,
+} from "../../app/models/conditionsRecord";
 import { Divider, capitalize } from "@mui/material";
 import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 import RecordsList from "../records/RecordsList";
@@ -28,7 +33,13 @@ type Props = {
 };
 
 const OrganDashboard = (props: Props) => {
-    const { organ, optionTitle, chartOption, chartOptions, onChangeChartOption } = props;
+    const {
+        organ,
+        optionTitle,
+        chartOption,
+        chartOptions,
+        onChangeChartOption,
+    } = props;
     const { t } = useTranslation("translation", { keyPrefix: "details" });
 
     const [range, setRange] = useState<DateRange>({
@@ -37,18 +48,26 @@ const OrganDashboard = (props: Props) => {
     });
 
     const rangeRecords = useMemo(
-        () => props.records.filter(r => range.start <= r.dateTime && r.dateTime <= range.end),
+        () =>
+            props.records.filter(
+                (r) => range.start <= r.dateTime && r.dateTime <= range.end
+            ),
         [props.records, range.end, range.start]
     );
 
     const rangeViolations = useMemo(
-        () => props.violations.filter(v => range.start <= v.record.dateTime && v.record.dateTime <= range.end),
+        () =>
+            props.violations.filter(
+                (v) =>
+                    range.start <= v.record.dateTime &&
+                    v.record.dateTime <= range.end
+            ),
         [props.violations, range.end, range.start]
     );
 
     const chartData = useMemo(
-        () => rangeRecords
-            .map<ConditionsRecordUnit>(r => ({
+        () =>
+            rangeRecords.map<ConditionsRecordUnit>((r) => ({
                 dateTime: r.dateTime,
                 value: Number(r[chartOption]),
             })),
@@ -76,7 +95,9 @@ const OrganDashboard = (props: Props) => {
                                     flexDirection: "column",
                                 }}
                             >
-                                <Typography align="center" variant="h5">{optionTitle} {t('chartName')}</Typography>
+                                <Typography align="center" variant="h5">
+                                    {optionTitle} {t("chartName")}
+                                </Typography>
                                 <ConditionsLineChart
                                     valueTitle={optionTitle}
                                     valueName={chartOption}
@@ -89,30 +110,12 @@ const OrganDashboard = (props: Props) => {
                                 />
                                 <DateRangePicker
                                     range={range}
-                                    onChange={range => setRange(range)}
+                                    onChange={(range) => setRange(range)}
                                 />
                             </Paper>
                         </Grid>
                         <Grid item md={5}>
-                            <Paper
-                                sx={{
-                                    p: 2,
-                                    display: "flex",
-                                    flexDirection: "column",
-                                }}
-                            >
-                                <Typography variant="h5" mb={2}>Organ Details</Typography>
-                                <Divider/>
-                                <Typography variant="h6" mt={1} mb={1}>Name: {organ.name}</Typography>
-                                <Divider/>
-                                <Typography variant="h6" mt={1} mb={1}>Description: {organ.description}</Typography>
-                                <Divider/>
-                                <Typography variant="h6" mt={1} mb={1}>Creation Date: {organ.organCreationDate.toString()}</Typography>
-                                <Divider/>
-                                <Typography variant="h6" mt={1} mb={1}>Container Name: {organ.container.name}</Typography>
-                                <Divider/>
-                                <Typography variant="h6" mt={1} mb={1}>Conditions Name: {organ.conditions.name}</Typography>
-                            </Paper>
+                            <OrganInfoCard organ={organ} />
                         </Grid>
                         <Grid item md={12}>
                             <Paper
@@ -145,6 +148,49 @@ const OrganDashboard = (props: Props) => {
 
 export default observer(OrganDashboard);
 
+type OrganInfoCardProps = {
+    organ: Organ;
+}
+
+const OrganInfoCard = (props: OrganInfoCardProps) => {
+    const { organ } = props;
+    const { t } = useTranslation("translation", { keyPrefix: "details.infoSection" });
+
+    return (
+        <Paper
+            sx={{
+                p: 2,
+                display: "flex",
+                flexDirection: "column",
+            }}
+        >
+            <Typography variant="h5" mb={2}>
+                {t('organDetails')}
+            </Typography>
+            <Divider />
+            <Typography variant="h6" mt={1} mb={1}>
+                {t('name')}: {organ.name}
+            </Typography>
+            <Divider />
+            <Typography variant="h6" mt={1} mb={1}>
+                {t('description')}: {organ.description}
+            </Typography>
+            <Divider />
+            <Typography variant="h6" mt={1} mb={1}>
+                {t('creationDate')}: {organ.organCreationDate.toString()}
+            </Typography>
+            <Divider />
+            <Typography variant="h6" mt={1} mb={1}>
+                {t('containerName')}: {organ.container.name}
+            </Typography>
+            <Divider />
+            <Typography variant="h6" mt={1} mb={1}>
+                {t('conditionsName')}: {organ.conditions.name}
+            </Typography>
+        </Paper>
+    );
+}
+
 export type ChartOptions = {
     opt: keyof ConditionsRecord;
     title: string;
@@ -158,66 +204,68 @@ type ChartOptionsSelectorProps = {
 
 const ChartOptionsSelector = (props: ChartOptionsSelectorProps) => {
     const { option, onChange } = props;
-    const { t } = useTranslation('translation', { keyPrefix: 'details' });
-    
-    const options = useMemo(
-        () => props.options.sort(),
-        [props.options]
-    );
+    const { t } = useTranslation("translation", { keyPrefix: "details" });
 
-    return <FormControl
-        required
-    >
-        <InputLabel>{t('condition')}</InputLabel>
-        <Select
-            label={t('condition')}
-            value={option}
-            onChange={(e) => onChange(e.target.value as keyof ConditionsRecord)}
-        >
-            {options.map(o => (
-                <MenuItem value={o.opt}>{o.title}</MenuItem>
-            ))}
-        </Select>
-    </FormControl>;
-}
+    const options = useMemo(() => props.options.sort(), [props.options]);
+
+    return (
+        <FormControl required>
+            <InputLabel>{t("condition")}</InputLabel>
+            <Select
+                label={t("condition")}
+                value={option}
+                onChange={(e) =>
+                    onChange(e.target.value as keyof ConditionsRecord)
+                }
+            >
+                {options.map((o) => (
+                    <MenuItem value={o.opt}>{o.title}</MenuItem>
+                ))}
+            </Select>
+        </FormControl>
+    );
+};
 
 type DateRange = {
     start: Date;
     end: Date;
-}
+};
 
 type DateRangePickerProps = {
     range: DateRange;
     onChange: (range: DateRange) => void;
-}
+};
 
 const DateRangePicker = (props: DateRangePickerProps) => {
     const { range, onChange } = props;
-    const { t } = useTranslation('translation', { keyPrefix: 'details' });
+    const { t } = useTranslation("translation", { keyPrefix: "details" });
 
     return (
         <Grid mt={1} container spacing={2}>
             <Grid item>
                 <DatePicker
-                    label={t('start')}
+                    label={t("start")}
                     value={dayjs(range.start)}
-                    onChange={val => val && onChange({ ...range, start: val.toDate() })}
+                    onChange={(val) =>
+                        val && onChange({ ...range, start: val.toDate() })
+                    }
                 />
             </Grid>
             <Grid item>
                 <DatePicker
-                    label={t('end')}
+                    label={t("end")}
                     value={dayjs(range.end)}
-                    onChange={val => val && onChange({ ...range, end: val.toDate() })}
+                    onChange={(val) =>
+                        val && onChange({ ...range, end: val.toDate() })
+                    }
                 />
             </Grid>
         </Grid>
     );
-}
+};
 
 function getDate(daysBefore: number): Date {
     const currentDate = new Date();
     currentDate.setDate(currentDate.getDate() - daysBefore);
     return currentDate;
 }
-
