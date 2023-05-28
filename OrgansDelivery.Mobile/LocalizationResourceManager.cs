cd47c4c6be4284173ecc9c2a61a3ad440ef4e9f4
Vitalii <vitaliinnest@gmail.com>
2023-05-28 15:07:27 +0300

    added some translation to mobile

---
+++ OrgansDelivery.Mobile/LocalizationResourceManager.cs
@@ -0,0 +1,25 @@
+﻿using OrgansDelivery.Mobile.Resources.Locales;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace OrgansDelivery.Mobile;
+
+public class LocalizationResourceManager : INotifyPropertyChanged
+{
+	public event PropertyChangedEventHandler PropertyChanged;
+	
+	private LocalizationResourceManager()
+	{
+		AppResources.Culture = CultureInfo.CurrentCulture;
+	}
+
+	public static LocalizationResourceManager Instance { get; } = new();
+	public object this[string resourceKey] =>
+		AppResources.ResourceManager.GetObject(resourceKey, AppResources.Culture) ?? Array.Empty<byte>();
+
+	public void SetCulture(CultureInfo culture)
+	{
+		AppResources.Culture = culture;
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
+	}
+}
--- OrgansDelivery.Mobile/MainPage.xaml.cs
+++ OrgansDelivery.Mobile/MainPage.xaml.cs
@@ -1,16 +1,31 @@
-﻿namespace OrgansDelivery.Mobile;
+﻿using OrgansDelivery.Mobile.Resources.Locales;
+using System.Globalization;
+
+namespace OrgansDelivery.Mobile;
 
 public partial class MainPage : ContentPage
 {
 	int count = 0;
 
+	public LocalizationResourceManager LocalizationResourceManager { get; } = LocalizationResourceManager.Instance;
+
 	public MainPage()
 	{
 		InitializeComponent();
+		BindingContext = this;
 	}
 
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
+		var switchToCulture = AppResources.Culture.TwoLetterISOLanguageName.ToLower() switch
+		{
+			"en" => new CultureInfo("uk-UA"),
+			"uk" => new CultureInfo("en-US"),
+			_ => throw new NotImplementedException()
+		};
+
+		LocalizationResourceManager.Instance.SetCulture(switchToCulture);
+
 		count++;
 
 		if (count == 1)
